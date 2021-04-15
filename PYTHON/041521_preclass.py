5eccb6eab593811bce9e83e192b5bf925ef0308a
fatihtepe <fatihtepe@gmail.com>
2021-04-15 14:55:29 -0400

    uncomment

--- PYTHON/041521_preclass.py
+++ PYTHON/041521_preclass.py
@@ -1,46 +1,46 @@
-# audience_group = 'kid', 'teen', 'adult'
-# audience = 'handsome'
-# if audience in audience_group:
-#     if audience == 'kid':
-#         print('it is free to go to cinema')
-#     elif audience == 'teen':
-#         print('discounted price!')
-#     else: 
-#         print('normal price')
-# else:
-    # print('No such audience, stay at your home!')
+audience_group = 'kid', 'teen', 'adult'
+audience = 'handsome'
+if audience in audience_group:
+    if audience == 'kid':
+        print('it is free to go to cinema')
+    elif audience == 'teen':
+        print('discounted price!')
+    else: 
+        print('normal price')
+else:
+    print('No such audience, stay at your home!')
 
 
-#score = int(input('Enter your score: '))
-# if score >= 90:
-#     if score >= 95:
-#         score_letter = 'A+' 
-#     else:
-#         score_letter = 'A'
-# elif score >= 80:
-#     if score >= 85:
-#         score_letter = 'B+'
-#     else:
-#         score_letter = 'B'
-# else:
-#     score_letter = 'below B'
+score = int(input('Enter your score: '))
+if score >= 90:
+    if score >= 95:
+        score_letter = 'A+' 
+    else:
+        score_letter = 'A'
+elif score >= 80:
+    if score >= 85:
+        score_letter = 'B+'
+    else:
+        score_letter = 'B'
+else:
+    score_letter = 'below B'
 
-# print('Your degree %s' % score_letter)
+print('Your degree %s' % score_letter)
 
 
-# country = input('What counry do you live in? ')
-# tax = 0
-# if country.lower() == 'canada':
-#     province = input('What province do you live in? ')
-#     if province.title() in ('Alberta', 'Yukon', 'Nunavut'):
-#         tax = 0.05
-#     elif province.title() == 'Ontario':
-#         tax = 0.13
-#     else:
-#         tax = 0.15
-# else:
-#     tax = 0.0
-# print(tax)
+country = input('What counry do you live in? ')
+tax = 0
+if country.lower() == 'canada':
+    province = input('What province do you live in? ')
+    if province.title() in ('Alberta', 'Yukon', 'Nunavut'):
+        tax = 0.05
+    elif province.title() == 'Ontario':
+        tax = 0.13
+    else:
+        tax = 0.15
+else:
+    tax = 0.0
+print(tax)
 
 
 # A student makes honour roll if their average is >=85
