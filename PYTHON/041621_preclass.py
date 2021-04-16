529f473a07e7ce5109b3b5e27d6eca96b250b838
fatihtepe <fatihtepe@gmail.com>
2021-04-16 10:08:59 -0400

    updates

--- PYTHON/041621_preclass.py
+++ PYTHON/041621_preclass.py
@@ -1,116 +1,116 @@
-# a = set('philadelphia')
-# b = set('fatihtepe')
+a = set('philadelphia')
+b = set('fatihtepe')
 
-# print(a - b)
-# print(a.difference(b))
+print(a - b)
+print(a.difference(b))
 
-# print(a | b)
-# print(a.union(b))
+print(a | b)
+print(a.union(b))
 
-# print(a & b) 
-# print(a.intersection(b))
+print(a & b) 
+print(a.intersection(b))
 
-# day1 = {'04/16/2021'}
-# day2 = set('04/16/2021')
+day1 = {'04/16/2021'}
+day2 = set('04/16/2021')
 
-# print(day1)
-# print(day2)
+print(day1)
+print(day2)
 
-# usa = set('washington')
-# nz = set('wellington')
+usa = set('washington')
+nz = set('wellington')
 
-# print(usa.intersection(nz))
-# print(usa.difference(nz))
-# print(usa.union(nz))
+print(usa.intersection(nz))
+print(usa.difference(nz))
+print(usa.union(nz))
 
-# if True:
-#     print('You can do that')
-
-
-# if False:
-#     print('nothing happened')
+if True:
+    print('You can do that')
 
 
-
-# minced_meat = True
-# hamburger_bread = True
-# lettuce = True
-# pepper = False
-# grocery_store = True
-
-# ingredients = minced_meat and hamburger_bread and grocery_store \
-#     and (lettuce or pepper)
-# if ingredients:
-#     print('Bon Appetit!')
+if False:
+    print('nothing happened')
 
 
 
+minced_meat = True
+hamburger_bread = True
+lettuce = True
+pepper = False
+grocery_store = True
 
-# empty_seat = int(input('please enter a number: '))
-# if empty_seat >= 3:
-#     print('There is an empty seat for you')
-
-
-
-# s1 = set('TWELVE PLUS ONE')
-# s2 = set('ELEVEN PLUS TWO')
-# if s1 == s2:
-#     print("Two sets are equal")
-
-
-# convert = input('Please write Yes or No: ').lower().strip() == 'yes'
-# print('You entered: ', convert)
-
-
-# print('You entered: ', input('Please write Yes or No: ').lower().strip() == 'yes')
-
-
-# num = int(input('Please enter a number: '))
-# if num % 2 == 0:  
-#     print(f'The {num} is even')
-# else:
-#     print(f'The {num} is odd')
-
-
-# posneg_num = float(input('Please enter a number: '))
-# if posneg_num > 0:
-#     print('positive number is entered')
-# else:
-#     print('negative number is entered')
-
-
-# n1 = int(input('please enter a number: '))
-# n2 = int(input('Please enter another number: '))
-# if n1 > n2: 
-#     larger = n1
-# else:
-#     larger = n2
-# print('The larger number is:', larger)
+ingredients = minced_meat and hamburger_bread and grocery_store \
+    and (lettuce or pepper)
+if ingredients:
+    print('Bon Appetit!')
 
 
 
-# n1 = int(input('please enter a number: '))
-# n2 = int(input('Please enter another number: '))
-# if n1 > n2:
-#     print('the larger nuber is', n1)
-# else:
-#     print('the larger number is', n2)
+
+empty_seat = int(input('please enter a number: '))
+if empty_seat >= 3:
+    print('There is an empty seat for you')
 
 
-# bool_value = True
-# if bool_value:
-#     print('Yes')
-# else:
-#     print('No')
+
+s1 = set('TWELVE PLUS ONE')
+s2 = set('ELEVEN PLUS TWO')
+if s1 == s2:
+    print("Two sets are equal")
 
 
-# first_num = int(input('Please enter a number: '))
-# second_num = int(input('Please enter another number: '))
-# third_num = int(input('Please enter last number: '))
+convert = input('Please write Yes or No: ').lower().strip() == 'yes'
+print('You entered: ', convert)
 
-# if first_num > second_num and first_num > third_num:
-#     print(f'the largest number is {first_num}')
-# elif second_num > third_num and second_num > first_num:
-#     print(f'the largest number is {second_num}')
-# else:
-#     print(f'the largest number is {third_num}')
+
+print('You entered: ', input('Please write Yes or No: ').lower().strip() == 'yes')
+
+
+num = int(input('Please enter a number: '))
+if num % 2 == 0:  
+    print(f'The {num} is even')
+else:
+    print(f'The {num} is odd')
+
+
+posneg_num = float(input('Please enter a number: '))
+if posneg_num > 0:
+    print('positive number is entered')
+else:
+    print('negative number is entered')
+
+
+n1 = int(input('please enter a number: '))
+n2 = int(input('Please enter another number: '))
+if n1 > n2: 
+    larger = n1
+else:
+    larger = n2
+print('The larger number is:', larger)
+
+
+
+n1 = int(input('please enter a number: '))
+n2 = int(input('Please enter another number: '))
+if n1 > n2:
+    print('the larger nuber is', n1)
+else:
+    print('the larger number is', n2)
+
+
+bool_value = True
+if bool_value:
+    print('Yes')
+else:
+    print('No')
+
+
+first_num = int(input('Please enter a number: '))
+second_num = int(input('Please enter another number: '))
+third_num = int(input('Please enter last number: '))
+
+if first_num > second_num and first_num > third_num:
+    print(f'the largest number is {first_num}')
+elif second_num > third_num and second_num > first_num:
+    print(f'the largest number is {second_num}')
+else:
+    print(f'the largest number is {third_num}')
