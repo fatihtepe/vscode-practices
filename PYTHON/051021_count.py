d8c93162f43b345508fc3a3d226f0dee0026ec50
fatihtepe <fatihtepe@gmail.com>
2021-05-10 09:18:29 -0400

    Count Occurrences in a Python List

---
+++ PYTHON/051021_count.py
@@ -0,0 +1,28 @@
+# Task: To find number of item within a list
+
+        
+numbers = [1,1,2,8,4,5,3,8,2,1,6,3,8,1,6,8]
+eights = []
+for x in numbers:
+    if x == 8:
+        eights.append(x)
+print(f'The number of eights: {len(eights)}')
+
+
+numbers = [1,1,2,8,4,5,3,8,2,1,6,3,8,1,6,8]
+eights = [x for x in numbers if x == 8]
+count_eights = len(eights)
+print(f'The number of eights: {count_eights}')
+
+
+numbers = [1,1,2,8,4,5,3,8,2,1,6,3,8,1,6,8]
+eights = list(filter(lambda x: x == 8, numbers))
+count_eights = len(eights)
+print(f'The number of eights: {count_eights}')
+
+
+numbers = [1,1,2,8,4,5,3,8,2,1,6,3,8,1,6,8]
+
+count_eights = numbers.count(8)
+
+print(f'The number of eights: {count_eights}')
