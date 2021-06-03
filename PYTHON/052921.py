9bf133ec361b7710eeaa8e9e55e261d185239463
fatihtepe <fatihtepe@gmail.com>
2021-06-03 14:06:30 -0400

    update

---
+++ PYTHON/052921.py
@@ -0,0 +1,45 @@
+import os
+os.system('clear')
+
+
+listem = [1, 2, 3]
+
+for i in listem:
+    for ii in listem:
+        for iii in listem:
+            if i != ii and i != iii and ii != iii:
+                print([i, ii,iii])
+               
+
+
+my_var = 101
+
+def simple_function():
+    print(my_var)
+    
+simple_function()
+
+
+new_var1 = 'My name is Jenkins'
+
+def my_funct():
+    new_var1 = 'my name is Javid'
+    print(new_var1)
+    
+my_funct()
+
+
+
+new_var1 = 'My name is Jenkins'
+
+def my_funct():
+    new_var1 = 'my name is Docker'
+    print(new_var1)
+    print(new_var1)
+    
+    
+print(new_var1)
+my_funct()
+print(new_var1)
+
+    
